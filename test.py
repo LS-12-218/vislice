af883da78c6f11fd801666342378b538e3d284f0
Luka Sajovic <luka.sajovic@student.fmf.uni-lj.si>
2019-04-24 14:59:21 +0200

    boljsa prastevila

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 prastevila = [2]
 trenutno = 3
-while trenutno < 200:
-    if not 0 in [trenutno % n for n in prastevila]:
+while trenutno <= 200:
+    if not 0 in [trenutno % n for n in prastevila if n <= trenutno ** 0.5]:
         prastevila.append(trenutno)
     trenutno += 2
 print(prastevila)
