3abe41f52a607f823f8b828c19bc5ccc49c8419f
Luka Sajovic <luka.sajovic@student.fmf.uni-lj.si>
2019-04-24 15:09:35 +0200

    Merge conflict experiment

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 prastevila = [2]
 trenutno = 3
-while trenutno <= 200:
+while trenutno <= 200000000000000000:
     if not 0 in [trenutno % n for n in prastevila if n <= trenutno ** 0.5]:
         prastevila.append(trenutno)
-    trenutno += 2
-print(prastevila)
+    trenutno += 1780
+print(prastevila)žrwesavsd
+tuzuzgmhki
